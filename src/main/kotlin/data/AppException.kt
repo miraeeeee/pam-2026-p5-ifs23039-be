4326303e07a0e86ac55f7aa26bb6fae131fae2df
miraeeeee <raynalllna70@gmail.com>
2026-03-08 17:50:53 +0700

    commit last

--- src/main/kotlin/data/AppException.kt
+++
@@ -1,9 +0,0 @@
-package org.delcom.data
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-open class AppException(
-    val code: Int,
-    override val message: String
-) : Exception(message)
--- src/main/kotlin/helpers/ContHelper.kt
+++
@@ -1,8 +0,0 @@
-package org.delcom.helpers
-
-object JWTConstants {
-    const val NAME = "auth-jwt"
-    const val REALM = "delcom-realm"
-    const val ISSUER = "delcom-app"
-    const val AUDIENCE = "delcom-user"
-}
